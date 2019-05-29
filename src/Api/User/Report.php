6cb7074e852ef6dff7efffcb422ba2ccae72822d
Daniel Costa <danielcosta@gmail.com>
2019-05-29 14:01:00 -0300

    Adding getBackupJobReportSummary on User::Report api

--- src/Api/User/Report.php
+++ src/Api/User/Report.php
@@ -34,4 +34,33 @@ class Report extends AbstractApi
 
         return $this->getClient()->request('GetBackupJobReport.do', $data);
     }
+
+    /**
+     * @return array
+     */
+    public function getBackupJobReportSummary(
+        string $login,
+        string $backupSetId,
+        string $backupJobId,
+        string $destinationId,
+        string $owner = null,
+        bool $cdp = null
+    ): array {
+        $data = [
+            'LoginName' => $login,
+            'BackupSetID' => $backupSetId,
+            'BackupJobID' => $backupJobId,
+            'DestinationID' => $destinationId,
+        ];
+
+        if (!is_null($owner)) {
+            $data['Owner'] = $owner;
+        }
+
+        if (!is_null($cdp)) {
+            $data['Cdp'] = $cdp;
+        }
+
+        return $this->getClient()->request('GetBackupJobReportSummary.do', $data);
+    }
 }
--- tests/Api/User/ReportTest.php
+++ tests/Api/User/ReportTest.php
@@ -21,4 +21,19 @@ class ReportTest extends AbstractTestCase
 
         $this->assertIsArray($response);
     }
+
+    public function testGetBackupJobReportSummary()
+    {
+        $api = new Report($this->getMockedClient());
+        $response = $api->getBackupJobReportSummary(
+            'username',
+            '1551985287886',
+            '2019-03-07-16-25-56',
+            '1551985385178',
+            'reseller',
+            true
+        );
+
+        $this->assertIsArray($response);
+    }
 }
